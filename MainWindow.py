16e306d52bb7306ce4357a44bba104f82e937b26
Erich-e <erich.elendt@gmail.com>
2017-01-11 16:12:52 -0500

    WIP Implimented adding, removing tasks. Tasks will now be stored in a .json file when the program exits

--- MainWindow.py
+++ MainWindow.py
@@ -1,15 +1,17 @@
 '''
 UI component for task managment
 '''
-import sys
-from PyQt4 import QtGui
-from tasks import task_list
 
+import sys
+import copy
+import datetime
+from PyQt4 import QtGui, QtCore
+from tasks import task_list, task, t_read, t_write
 
 class mainWindow(QtGui.QMainWindow):
 	def __init__(self, parent=None):
 		QtGui.QWidget.__init__(self, parent)
-		self.tasks = task_list()
+		self.load()
 		self.init_ui()
 
 	def init_ui(self):
@@ -17,41 +19,121 @@ class mainWindow(QtGui.QMainWindow):
 		with open("./QTDark.stylesheet") as f:
 			self.setStyleSheet(f.read())
 		self.layout = QtGui.QHBoxLayout()
-		self.task_display = QtGui.QAbstractScrollArea()
 		self.menu_display = QtGui.QVBoxLayout()
+		self.task_display = None
 
-		self.task_layout = QtGui.QGridLayout()
-		self.task_layout.addWidget(QtGui.QLabel("Task: "), 0, 0)
+		self.update_display()
+
+		self.menu_layout = QtGui.QGridLayout()
+		self.menu_layout.addWidget(QtGui.QLabel("Task: "), 0, 0)
 		self.title_edit = QtGui.QLineEdit()
-		self.task_layout.addWidget(self.title_edit, 0, 1)
+		self.menu_layout.addWidget(self.title_edit, 0, 1)
 
-		self.task_layout.addWidget(QtGui.QLabel("Description: "), 1, 0)
+		self.menu_layout.addWidget(QtGui.QLabel("Description: "), 1, 0)
 		self.desc_edit = QtGui.QTextEdit()
-		self.task_layout.addWidget(self.desc_edit, 1, 1)
+		self.menu_layout.addWidget(self.desc_edit, 1, 1)
 
-		self.task_layout.addWidget(QtGui.QLabel("Due Date: "), 2, 0)
+		self.menu_layout.addWidget(QtGui.QLabel("Due Date: "), 2, 0)
 		self.date_edit = QtGui.QCalendarWidget()
-		self.task_layout.addWidget(self.date_edit, 2, 1)
+		self.menu_layout.addWidget(self.date_edit, 2, 1)
 
 		self.submit_button_layout = QtGui.QHBoxLayout()
-		self.task_button = QtGui.QPushButton("New Task")
-		self.submit_button_layout.addWidget(self.task_button)
+		self.submit_button = QtGui.QPushButton("New Task")
+		self.submit_button_layout.addWidget(self.submit_button)
 		self.submit_button_layout.addStretch(1)
-		self.task_layout.addLayout(self.submit_button_layout, 3, 1)
+		self.menu_layout.addLayout(self.submit_button_layout, 3, 1)
+		self.submit_button.clicked.connect(self.insert_task)
 
-		self.menu_display.addLayout(self.task_layout)
+		self.menu_display.addLayout(self.menu_layout)
 		self.menu_display.addStretch(1)
 
-		self.layout.addWidget(self.task_display)
 		self.layout.addLayout(self.menu_display)
 
 		cwidget = QtGui.QWidget()
 		cwidget.setLayout(self.layout)
 		self.setCentralWidget(cwidget)
 
+	def update_display(self):
+		if self.tasks.size != 0:
+			display_layout = QtGui.QVBoxLayout()
+			# for task in self.tasks:
+			for i in range(self.tasks.size):
+				next_block = QtGui.QGroupBox()
+				block_layout = QtGui.QVBoxLayout()
+
+				title_row = QtGui.QHBoxLayout()
+				# title_row.addWidget(QtGui.QLabel(task.title))
+				title_row.addWidget(QtGui.QLabel(self.tasks.access(i).title))
+				title_row.addStretch(1)
+				remove_btn = QtGui.QPushButton('X')	
+				title_row.addWidget(remove_btn)
+				block_layout.addLayout(title_row)
+
+				desc_row = QtGui.QHBoxLayout()
+				# desc_row.addWidget(QtGui.QLabel(task.description))
+				desc_row.addWidget(QtGui.QLabel(self.tasks.access(i).description))
+				desc_row.addStretch(1)
+				# desc_row.addWidget(QtGui.QLabel(task.date_str()))
+				desc_row.addWidget(QtGui.QLabel(self.tasks.access(i).date_str()))
+				block_layout.addLayout(desc_row)
+
+				next_block.setLayout(block_layout)
+				display_layout.addWidget(next_block)
+
+				remove_btn.clicked.connect(lambda: self.remove_task(self.tasks.access(i)))
+
+			display_layout.addStretch(1)
+			inner_wrapper = QtGui.QWidget()
+			inner_wrapper.setLayout(display_layout)
+		else:
+			inner_wrapper = QtGui.QWidget()
+
+		if self.task_display:
+			self.layout.removeWidget(self.task_display)
+			self.task_display.deleteLater()
+		self.task_display = QtGui.QScrollArea()
+		inner_wrapper.setMaximumWidth(self.task_display.width())
+		self.task_display.setWidget(inner_wrapper)
+		self.task_display.setWidgetResizable(True)
+		self.layout.insertWidget(0, self.task_display)
+
+
+	def remove_task(self, task=None, title="", description="", due_date=None):
+		print task.title
+		self.tasks.remove(old_task=task,
+						  title=title,
+						  description=description,
+						  due_date=due_date)
+		self.update_display()
+
+	def insert_task(self):
+		title = str(self.title_edit.text())
+		if title:
+			desc = str(self.desc_edit.toPlainText())
+			qt_date = self.date_edit.selectedDate()
+			due_date = datetime.datetime.strptime(str(qt_date.toString(QtCore.Qt.ISODate)), "%Y-%m-%d").date()
+			self.tasks.insert(title=title, description=desc, due_date=due_date)
+			self.update_display()
+			self.title_edit.clear()
+			self.desc_edit.clear()
+
+	def load(self):
+		try:
+			self.tasks = t_read("save.json")
+		except:
+			self.tasks = task_list()
+			err = QtGui.QErrorMessage()
+			err.showMessage("Corrupted save file")
+			err.exec_()
+
+	def clean_up(self):
+		t_write("save.json", self.tasks)
+
+
 if __name__ == '__main__':
 	app = QtGui.QApplication(sys.argv)
 	window = mainWindow()
 	window.show()
+	app.lastWindowClosed.connect(window.clean_up)
 	app.exec_()
 	sys.exit()
--- tasks.py
+++ tasks.py
@@ -10,6 +10,17 @@ class task():
 		self.title = title
 		self.description = description
 		self.due_date = due_date
+		# self.due_date = datetime.date.today()
+
+	def date_str(self):
+		return self.due_date.strftime("%a, %b %d")
+
+	def __eq__(self, other):
+		return (self.title == other.title) and (self.description == other.description) \
+			and (self.due_date == other.due_date)
+
+	def __ne__(self, other):
+		return not self.__eq__(other)
 
 class task_node():
 	def __init__(self, data=None, next=None):
@@ -28,38 +39,46 @@ class task_list():
 			self.size = 0
 
 	def __iter__(self):
+		self.iter_tmp = self.root
+		self.iter_tmp_size = self.size
 		return self
 
 	def next(self):
-		if not self.root.next():
+		if not self.root:
+			self.root = self.iter_tmp
+			self.size = self.iter_tmp_size
 			raise StopIteration
 		else:
 			t = self.root.data
 			self.remove(t)
 			return t
 
-	def get_size(self):
-		return self.size
-
-	def insert(self, task=None):
+	def insert(self, new_task=None, title="", description="", due_date=None):
 		self.size += 1
-		if not self.root or self.root.data.date > task.date:
-			self.root = task_node(data=task, next=self.root)
+		if not new_task:
+			new_task = task(title=title, description=description, due_date=due_date)
+		if not self.root or self.root.data.due_date > new_task.due_date:
+			self.root = task_node(data=new_task, next=self.root)
 		else:
 			cur = self.root
-			while cur.next and cur.data.date <= task.date:
+			while cur.next and cur.next.data.due_date <= new_task.due_date:
 				cur = cur.next
-			cur.next = task_node(date=task, next=cur.next)
+			cur.next = task_node(data=new_task, next=cur.next)
 
-	def remove(self, task=None):
+	def remove(self, old_task=None, title="", description="", due_date=None):
+		if self.size == 0:
+			return
 		self.size -= 1
-		if self.root == task:
+		if not old_task:
+			old_task = task(title=title, description=description, due_date=due_date)
+		if self.root.data == old_task:
 			self.root = self.root.next
-		cur = self.root
-		while cur.next and cur.next.data != task:
-			cur = cur.next
-		if cur:
-			cur.next = cur.next.next
+		else:
+			cur = self.root
+			while cur.next and cur.next.data != old_task:
+				cur = cur.next
+			if cur:
+				cur.next = cur.next.next
 
 	def search(self, val=None, param=None):
 		cur = self.root
@@ -67,6 +86,17 @@ class task_list():
 			if getattr(cur.data, param, None) == param:
 				return cur.data
 		return None
+
+	def access(self, index=0):
+		if not self.root or index < 0:
+			print "index out of range"
+			raise
+		else:
+			cur = self.root
+			while index > 0:
+				index -= 1
+				cur = cur.next
+			return cur.data
 
 	def to_json(self):
 		data = []
@@ -77,22 +107,28 @@ class task_list():
 			cur = cur.next
 		return data
 
-def read(filename=""):
-	fpath = os.path.dirname(os.path.abspath(__file__)) + filename
+def t_read(filename=""):
+	fpath = os.path.dirname(os.path.abspath(__file__)) + '/' + filename
 	if not os.path.isfile(fpath):
 		return task_list()
 	else:
 		tlist = task_list()
 		with open(fpath) as f:
-			data = json.load(f)
+			try:
+				data = json.load(f)
+			except:
+				raise
 		for e in data:
-			t = task(title=e["title"], description=e["description"], due_date=datetime.datetime.strptime(e["due_date"], "%Y-%m-%d").date())
+			date_obj = datetime.datetime.strptime(e["due_date"], "%Y-%m-%d").date()
+			t = task(title=e["title"],
+				description=e["description"],
+				due_date=date_obj)
 			tlist.insert(t)
 		return tlist
 
-def write(filename="", tlist=None):
+def t_write(filename="", tlist=None):
 	if tlist:
 		data = tlist.to_json()
-		fpath = os.path.dirname(os.path.abspath(__file__)) + filemane
+		fpath = os.path.dirname(os.path.abspath(__file__)) + '/' + filename
 		with open(fpath, "w+") as f:
 			json.dump(data, f)
